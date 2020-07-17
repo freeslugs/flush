08a0edcaf2cf9373acb541fac373424d28988e61
Gilad Penn <freeslugs@gmail.com>
2020-07-17 12:55:58 -0400

    colors!

--- flush.sh
+++ flush.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 
+PURPLE='\033[0;35m'
+RED='\033[0;31m'
+ORANGE='\033[0;33m'
+CYAN='\033[0;36m'
+NOCOLOR='\033[0m'
+
 # kill lingering rails server
 function flush {
 	if [[ -f ./tmp/pids/server.pid ]]
 	then
 	  DEAD_PORT=$(<./tmp/pids/server.pid)
-    
     if ps -p $DEAD_PORT | grep -q $DEAD_PORT 
     then 
       kill -9 $(<./tmp/pids/server.pid)
-      echo "Removed server on port $DEAD_PORT." 
+      echo "${CYAN}Removed server on port ${DEAD_PORT}${NOCOLOR}." 
     else
-      echo 'Server is aleady dead.'
+      echo "${ORANGE}Server is aleady ${RED}dead${NOCOLOR}."
     fi
 	else
-	  echo 'No server running in this directory.'
+	  echo "${PURPLE}No server running in this directory${NOCOLOR}."
 	fi
 }
 
