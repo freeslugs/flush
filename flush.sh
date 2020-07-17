a17ad9ddcb8e0ad1ae6a142ea60e874c6d876555
Gilad Penn <freeslugs@gmail.com>
2020-07-17 11:22:49 -0400

    init flush

---
+++ flush.sh
@@ -0,0 +1,17 @@
+# kill lingering rails server
+function flush {
+	if [[ -f ./tmp/pids/server.pid ]]
+	then
+	  DEAD_PORT=$(<./tmp/pids/server.pid)
+    
+    if ps -p $DEAD_PORT | grep -q $DEAD_PORT 
+    then 
+      kill -9 $(<./tmp/pids/server.pid)
+      echo "Removed server on port $DEAD_PORT." 
+    else
+      echo 'Server is aleady dead.'
+    fi
+	else
+	  echo 'No server running in this directory.'
+	fi
+}
